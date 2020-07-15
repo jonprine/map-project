cb31644859c10deabc572a0fa94770c7b2e13c80
Vox Humana <jonprine@gmail.com>
2020-07-15 11:20:57 -0500

    added content tags to multiple markers

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function initMap() {
     var options = {
         zoom: 8,
-        center: {lat:36.1627,lng:-86.7816}
+        center: { lat: 36.1627, lng: -86.7816 }
     }
 
     // new map
 
-    var map = new 
-    google.maps.Map(document.getElementById("map"), options);
+    var map = new
+        google.maps.Map(document.getElementById("map"), options);
 
     // add marker
     /*
@@ -27,16 +27,43 @@ function initMap() {
 
     */
 
-    addMarker({lat:36.1757,lng:-86.7556});
-    addMarker({lat:33.7410,lng:-84.3459});
-    addMarker({lat:40.7206,lng:-73.9939});
+    addMarker({
+        coords: { lat: 36.1757, lng: -86.7556 },
+        iconImage: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+        content: "<h3>Basement East</h3>"
+    });
+    addMarker({ 
+        coords: { lat: 33.7410, lng: -84.3459 },
+        content: "<h3>The Earl</h3>"
+     });
+    addMarker({ 
+        coords: { lat: 40.7206, lng: -73.9939 },
+        content: "<h3>Bowery Ballroom</h3>"
+    });
 
     // add marker function
-    function addMarker(coords) {
+    function addMarker(props) {
         var marker = new google.maps.Marker({
-            position:coords,
-            map:map,
-            icon:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+            position: props.coords,
+            map: map,
+            //icon:props.iconImage,
         });
+
+        // check for customicon
+        if (props.iconImage) {
+            // set icon image
+            marker.setIcon(props.iconImage);
+        }
+
+        // check content 
+        if (props.content) {
+            var infoWindow = new google.maps.InfoWindow({
+                content: props.content
+            });
+
+            marker.addListener("click", function () {
+                infoWindow.open(map, marker);
+            });
+        }
     }
 }
